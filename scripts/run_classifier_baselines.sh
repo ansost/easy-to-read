b7d8d41eac5b52ec91ce33a024380e0dceaa27a4
ansost <anna.stein@hhu.de>
2024-05-10 14:28:19 +0200

    add util shell script for classifier baselines

---
+++ scripts/run_classifier_baselines.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for classifier in "RF" "MLP" "SVM"
+do
+    for value in "True" "False"
+    do
+        python3 classifier_baselines.py \
+            --classifier $classifier \
+            --dataset "train" \
+            --clean $value 
+    done
+done
