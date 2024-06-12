b81f706a90cc50be000c081be4721aa318088e3f
ansost <anste145@hhu.de>
2024-06-12 10:38:42 +0200

    update classifier baselines

--- scripts/classifier_baselines.py
+++ scripts/classifier_baselines.py
@@ -2,45 +2,19 @@
 Presupposes that your dataset is in the data/metrics/.
 
 Usage:
-    python classifier_baselines.py --dataset <dataset> --clean <clean> --classifier <classifier>"""
+    python classifier_baselines.py --dataset <dataset> --clean <clean> --classifier <classifier> --zero_class <zero_class>"""
 
 import pandas as pd
 import argparse
-from transformers import pipeline
-from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
+
 from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-from . import embeddings
+# import embeddings
+# import classifiers
 from embeddings import *
-
-
-def get_embeddings(phrase: str):
-    return pipe(phrase)[0]["token_str"]
-
-
-def clean_data(data: pd.DataFrame) -> pd.DataFrame:
-    """Remove original train dataset columns from df."""
-    out = [
-        "sent-id",
-        "topic",
-        "phrase",
-        "phrase_number",
-        "genre",
-        "timestamp",
-        "user",
-        "phrase_tokenized",
-        "statement_spans",
-        "notes",
-        "pos_onehot",
-    ]
-    cols = data.columns
-    for name in out:
-        if name in cols:
-            data.drop(columns=[name], inplace=True)
-    return data
+from classifiers import *
 
 
 if __name__ == "__main__":
@@ -54,69 +28,58 @@ if __name__ == "__main__":
     parser.add_argument(
         "--clean",
         type=bool,
-        required=True,
+        required=False,
         help="'True' or 'False'.Whether to remove the columns present in the original train dataset, like the tokenized phrase, notes, etc.",
     )
     parser.add_argument(
         "--classifier",
         type=str,
         required=True,
-        help="Which classifier to use. Options: Random Forrst 'RF', Multi-layer Perceptron 'MLP'",
+        help="Which classifier to use. Options: Random Forrst 'RF', Multi-layer Perceptron 'MLP', Support Vector Machine 'SVM', or logistic regression 'regression'.",
     )
     parser.add_argument(
-        "--add_embeddings",
+        "--zero_class",
         type=str,
-        required=True,
-        default="True",
-        help="Whether to add embeddings to the df. Default: True.",
+        default="keep",
+        required=False,
+        help="Whether to keep or remove the 0 labels ('keep', 'remove'). Default: keep.",
     )
     args = parser.parse_args()
-
-    classifiers = {
-        "RF": RandomForestClassifier,
-        "MLP": MLPClassifier,
-        "SVM": SVC,
-    }
 
     assert args.classifier in [
         "RF",
         "MLP",
         "SVM",
-    ], "Invalid classifier (RF, MLP, SVM are available)."
+        "regression",
+    ], "Invalid classifier (RF, MLP, SVM, regression are available)."
 
     data = pd.read_csv(
         f"../data/metrics/{args.dataset}_basics.csv",
         dtype={"num_statements": str, "notes": str},
     )
+    data = data.groupby("num_statements").filter(lambda x: len(x) > 3)
 
-    if args.embeddings == "True":
-        if "embeddings" not in data.columns:
-            pipe = pipeline("fill-mask", model="google-bert/bert-base-german-cased")
-            data["embeddings"] = data["phrase"].apply(get_embeddings)
-            data.to_csv(f"../data/metrics/{args.dataset}.csv", index=False)
+    if args.zero_class == "remove":
+        train = data[data["num_statements"] != "0"]
+        labels = train["num_statements"]
+    else:
+        train = data
+        labels = data["num_statements"]
+    train = train.select_dtypes(include=["number"])
 
-    data = data.groupby("num_statements").filter(lambda x: len(x) > 1)
+    scaler = StandardScaler()
+    train = pd.DataFrame(scaler.fit_transform(train), columns=train.columns)
 
-    labels = data["num_statements"]
-    train = data.select_dtypes(include=["number"])
-    print(
-        f"{args.classifier} is being trained using: \n{train.columns} \nParameters: {args}"
-    )
-
+    print(f"Results from {args.classifier.upper()} using: {list(train.columns)}")
     if args.clean == "True":
-        train = clean_data(train)
+        train.drop(columns=["sent-id"], inplace=True)
 
     X_train, X_test, y_train, y_test = train_test_split(
         train, labels, test_size=0.33, random_state=42, stratify=labels
     )
 
-    clf = classifiers[args.classifier](random_state=1).fit(X_train, y_train)
-    preds = clf.predict(X_test)
-
-    if args.classifier == "RF":
-        print(clf.feature_importances_)
+    preds, weights = eval(args.classifier)(X_train, X_test, y_train)
 
     print("Classification score:\n")
+    print(weights)
     print(classification_report(y_test, preds, zero_division=0))
-    print("Crossvalidation score:\n")
-    print(cross_val_score(clf, train, labels, cv=3, scoring="accuracy"))
